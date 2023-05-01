69fabf82a9236d4319ed7d9ece07ab32c75bc412
king <102906132+Ggy-king@users.noreply.github.com>
2023-05-01 09:41:30 +0800

    Add files via upload

---
+++ cesiummyapp/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="cesiumContainer"></div>
+</template>
+
+<script setup>
+import * as Cesium from 'cesium';
+import { onMounted } from 'vue';
+onMounted(() => {
+  const viewer = new Cesium.Viewer('cesiumContainer');
+
+})
+</script>
+
+
+<style scoped>
+  #cesiumContainer {
+    width: 100%;
+    height: 100%;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+  }
+</style>
